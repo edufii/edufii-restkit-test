833d84a4c550f84fff7c6c7d81b02c3a7e593133
Xuan Nguyen <darth.xuan@gmail.com>
2012-09-16 22:08:09 -0600

    Initial Commit

---
+++ edufii/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  edufii
+//
+//  Created by Xuan Nguyen on 9/16/12.
+//  Copyright (c) 2012 edufii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ edufii/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  edufii
+//
+//  Created by Xuan Nguyen on 9/16/12.
+//  Copyright (c) 2012 edufii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
